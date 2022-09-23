391889cf133b373d5c16425b155a7bb0064f2541
JaeHeong <rlawogud970301@gmail.com>
2022-09-23 17:46:01 +0900

    paging
    paging

--- project/myWeb/blogapp/views.py
+++ project/myWeb/blogapp/views.py
@@ -44,3 +44,13 @@ def boardDelete(request, pk):
     board = Board.objects.get(id=pk)
     board.delete()
     return redirect('board')
+
+from django.core.paginator import Paginator
+
+def test(request):
+    board_list = Board.objects.all() #models.py Board 클래스의 모든 객체를 board_list에 담음
+    # board_list 페이징 처리
+    page = request.GET.get('page', '1') #GET 방식으로 정보를 받아오는 데이터
+    paginator = Paginator(board_list, '10') #Paginator(분할될 객체, 페이지 당 담길 객체수)
+    page_obj = paginator.page(page) #페이지 번호를 받아 해당 페이지를 리턴 get_page 권장
+    return render(request, 'template_name', {'page_obj':page_obj}) 
